6ed3f5c8807e65934445fadeb825282718ead4fc
salvy-greek <4079753@itisrossi.vi.it>
2022-10-03 22:32:58 +0200

    vado avanti(eric) -_-

--- src/script.js
+++ src/script.js
@@ -1,3 +1,10 @@
+// var btnmenustyle=document.getElementsByClassName("btnmenu").item(0).className;
+// (function Selected(e){
+
+//     document.getElementsByClassName("btnmenu").item(0).className += " bg-red-600";
+// })()
+
+
 var map = L.map('mappa').setView([51.505, -0.09], 13);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
