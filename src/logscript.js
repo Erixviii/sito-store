6db754ddf39a59883202420cb399e377d4999779
salvy-greek <4079753@itisrossi.vi.it>
2022-10-06 06:28:50 +0200

    .

---
+++ src/logscript.js
@@ -0,0 +1,22 @@
+function Login(){
+
+    fetch('https://dummyjson.com/auth/login', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({
+        
+        username: document.getElementById("username").value,
+        password: document.getElementById("password").value,
+        expiresInMins: 60
+    })
+    })
+    .then(res => res.json())
+    .then(mex => {
+        if(mex.message=="Invalid credentials"){
+            window.location.href = "http://127.0.0.1:5500/src/home.html";
+        }
+        else {
+            alert("credenziali sbagliate");
+        }
+    });
+}
--- src/script.js
+++ src/script.js
@@ -14,3 +14,33 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 L.marker([51.5, -0.09]).addTo(map)
     .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
     .openPopup();
+
+
+    function Login(){
+
+        fetch('https://dummyjson.com/auth/login', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+            
+            username: document.getElementById("username").value,
+            password: document.getElementById("password").value,
+            expiresInMins: 60
+        })
+        })
+        .then(res => res.json())
+        .then(mex => {
+            if(mex.message=="Invalid credentials"){
+                window.location.href = "http://127.0.0.1:5500/src/home.html";
+            }
+            else {
+                alert("credenziali sbagliate");
+            }
+        });
+    }
+    function Eric(){
+
+        
+        window.location.href = "http://127.0.0.1:5500/src/login.html";
+    }
+    
